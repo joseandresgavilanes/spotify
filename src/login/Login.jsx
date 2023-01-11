0adb9bc9322458fac8643ff82157123b4cfc9652
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-11 17:55:08 -0500

    commit  inical login

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password",
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const handleSubmit = (event) => {
+    const errors = {
+      uname: "invalid username",
+      pass: "invalid password",
+    };
+  };
+
+  return (
+    <div>
+      <div className="form" onSubmit={handleSubmit}>
+        <form>
+          <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+            {renderErrorMessage("uname")}
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+            {renderErrorMessage("pass")}
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
