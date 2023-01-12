697b2b010ee302faea57a69d5a7ed474acae391b
magobasti <abastidas94@gmail.com>
2023-01-12 14:59:16 -0500

    Subida de fotos y rellenado de datos

--- src/about/tarjeta/Tarjeta.jsx
+++ src/about/tarjeta/Tarjeta.jsx
@@ -8,19 +8,21 @@ const Tarjeta = (props) => {
 
         <div className='container'>
             <div class="card-vinyl">
-    <div class="card-image" style={{ backgroundImage: `url(${imgUrl})` }}></div>
+                <div className='card_image_container'>
+                <div class="card-image" style={{ backgroundImage: `url(${imgUrl})` }}></div>
+                </div>
                 <div class="card-content">
                     <h2 class="card-title">{props.nombre}</h2>
                     <p class="card-artist">{props.puesto}</p>
                     <div className='iconos'>
                         <div className='iconos_redes'>
-                            <a href={props.linkG}><i class="fa-brands fa-github"></i></a>
+                            <a href={props.linkG} Target="_blank"><i class="fa-brands fa-github"></i></a>
                         </div>
                         <div className='iconos_redes'>
-                            <a href={props.linkl}><i class="fa-brands fa-linkedin"></i></a>
+                            <a href={props.linkl} Target="_blank"><i class="fa-brands fa-linkedin"></i></a>
                         </div>
                         <div className='iconos_redes'>
-                            <a href={props.linki}><i class="fa-brands fa-instagram"></i></a>
+                            <a href={props.linki} Target="_blank"><i class="fa-brands fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
@@ -30,7 +32,7 @@ const Tarjeta = (props) => {
                     </div>
                 </div>
                 <div className='info'>
-                <p >{props.detalle}</p>
+                    <p className='texto'>{props.detalle}</p>
                 </div>
             </div>
         </div>
