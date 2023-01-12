1c7a7fb465c3aaf290770b431c01bd10beb20a04
magobasti <abastidas94@gmail.com>
2023-01-12 17:45:37 -0500

    Animación de entrada

--- src/about/tarjeta/Tarjeta.jsx
+++ src/about/tarjeta/Tarjeta.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import "./Tarjeta.scss";
 
 const Tarjeta = (props) => {
+    const [entrando, setEntrando] = useState(false);
 
+    useEffect(() => {
+      setEntrando(true);
+    }, []);
+  
     const imgUrl = props.foto;
     return (
 
-        <div className='container'>
+        <div className={`mi-componente ${entrando ? 'mi-componente-entrando' : ''}`}>
             <div class="card-vinyl">
                 <div className='card_image_container'>
                 <div class="card-image" ><img className='fotos_about' src={imgUrl}/></div>
