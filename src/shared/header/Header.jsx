237417eaa3ccc1a61d5e5f94e9c2b7569891fbce
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-12 14:00:03 -0500

    actualizar  rama  login

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -117,6 +117,24 @@ const Header = () => {
             My Playlist #3
           </NavLink>
         </div>
+      {/* agregado para  elimnar  luego */}
+      <NavLink className="header_links_item" to="/login">
+            <svg
+              role="img"
+              height="24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z"
+                fill="currentColor"
+              ></path>
+            </svg>
+            Login
+          </NavLink>
+          
+
       </div>
     </header>
   );
