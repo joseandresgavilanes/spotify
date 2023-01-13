bfaea36ba4d8a6b251cce247c34f5036775b22d8
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-13 11:45:04 -0500

    redux

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <NavLink className="header_logo" to="/">
           <img
             className="header_logo-img"
-            src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+            src="./images/krugerlogo.png"
             alt=""
           />
         </NavLink>
