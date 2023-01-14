16fd8fd463a24e569d51955d83405e1dedda4a7e
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-14 13:53:17 -0500

    register funcionando sin  css y falta validar  el correo electronico exite

--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [nombre, setNombre] = useState("");
@@ -8,6 +10,15 @@ const Register = () => {
     const [correo, setCorreo] = useState("");
     const [contraseña, setContraseña] = useState("");
     const [foto, setFoto] = useState("");
+    const [fotopre, setFotopre] = useState("");
+   /*  const [registration, setRegistration] = useState({ 
+        nombre :'',
+        artistname :'',
+        apellido :'',
+        correo :'',
+        contraseña :'',
+        foto :'',});     */
+    const navigate = useNavigate();
 
     function handleChange(e) {
       const name = e.target.name;
@@ -24,7 +35,7 @@ const Register = () => {
               setCorreo(value);
               break;
           case 'contraseña':
-              setContraseña(e.target.checked);
+              setContraseña(value);
               break;
           case 'artistname':
               setArtistname(value);
@@ -34,10 +45,53 @@ const Register = () => {
       }
     }
 
-    function handleSubmit(e) {}
+    function handleFile(e) {
+      const element = e.target;
+      const file = element.files[0];
+      if(!file ){
+        alert('you must upload photo')
+      }
+     const reader = new FileReader();
+     reader.readAsDataURL(file);
+     reader.onloadend = function () {
+     const base64 = reader.result;  
+     console.log(base64.split(','));
+     setFotopre(base64);
+     const auxiliar = base64.split(',');
+     console.log(auxiliar[1]);
+     setFoto( auxiliar[1]);    
+     }
+  }
 
-    function handleFile(e) {}
+  function handleSubmit(e) {
+    e.preventDefault();
 
+    const registration = {
+      nombre,
+      artistname,
+      apellido,
+      correo,
+      contraseña,
+      foto,
+    };
+    //TODO para crear
+    console.log(registration);
+    console.log(registration.contraseña);
+    if (registration) {
+      try {
+        const response = axios({
+          url: `https://loggin-api-production.up.railway.app/usuarios`,
+          method: "POST",
+          data: registration,
+        });
+        console.log(response);
+        navigate("/");
+      } catch (e) {
+        console.log(e);
+      }
+      //store.createItem(newBook);
+    }
+  }
 
   return (
     <div>
@@ -45,28 +99,28 @@ const Register = () => {
     <form onSubmit={handleSubmit} className="">
                 <div className= "">
                     <div className="" >Nombre</div>
-                    <input className="" type='text' name='nombre' onChange={handleChange} value={nombre} />
+                    <input className="" type='text' name='nombre' required onChange={handleChange} value={nombre} />
                 </div>
                 <div className= "">
                     <div className="">Apellido</div>
-                    <input className="" type='text' name='apellido' onChange={handleChange} value={apellido} />
+                    <input className="" type='text' name='apellido' required onChange={handleChange} value={apellido} />
                 </div>
                 <div className= "">
                     <div className="">Artist Name</div>
-                    <input className="" type='text' name='artistname' onChange={handleChange} value={artistname} />
+                    <input className="" type='text' name='artistname' required onChange={handleChange} value={artistname} />
                 </div>
                 <div className= "">
                     <div className="">E-mail</div>
-                    <input className="" type='text' name='correo' onChange={handleChange} value={correo} />
+                    <input className="" type='text' name='correo' required onChange={handleChange} value={correo} />
                 </div>
                 <div className= "">
                     <div className="">Password</div>
-                    <input className="" type='password' name='contraseña' onChange={handleChange} value={contraseña} />
+                    <input className="" type='password' name='contraseña' required onChange={handleChange} value={contraseña} />
                 </div>
                 <div className= "" >
-                    <div className="" >Foto</div>
-                    <input className="" type='file' name='foto' onChange={handleFile} />
-                    <div>{!!foto ? <img src={foto} width='200' alt='preview' /> : ''}</div>
+                    <div className="" >Photo</div>
+                    <input className="" type='file' name='foto' required onChange={handleFile} />
+                    <div>{!!foto ? <img src={fotopre} width='270' alt='preview' /> : ''}</div>
                 </div>
                 <input
                     className=""
