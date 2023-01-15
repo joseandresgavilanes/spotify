04a9f4006ff7f525faed6c8ed304459648fb00b5
magobasti <abastidas94@gmail.com>
2023-01-14 21:32:53 -0500

    arreglo de api

--- src/home/aboutMe/AboutMe.jsx
+++ src/home/aboutMe/AboutMe.jsx
@@ -5,11 +5,10 @@ import { NavLink } from "react-router-dom";
 
 const AboutMe = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(storedUser)
   return (
     <NavLink to="/AboutMe">
     <div className="aboutme">
-      <i class="fa-solid fa-user"></i>
+    <img className="rounded-circle" src={`data:image/jpeg;base64,${storedUser.foto}`} alt="User Photo" />
       <p>{storedUser.nombre+" "}{storedUser.apellido}</p>
     </div>
     </NavLink>
