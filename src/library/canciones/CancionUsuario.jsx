3bae22be54caa5470924418eaedbd5559d7dfe2a
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-15 22:53:42 -0500

    details styles

--- src/library/canciones/CancionUsuario.jsx
+++ src/library/canciones/CancionUsuario.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import '../../home/songs/song/Song.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../home/songs/song/Song.scss";
 
 const CancionUsuario = (props) => {
-    console.log(props.user)
+  console.log(props.user);
 
-    localStorage.setItem('cancion',JSON.stringify(props.user.titulo));
+  localStorage.setItem("cancion", JSON.stringify(props.user.id));
 
-    return (
-<NavLink to={`/songsDetails`}>
-          <div className="card">
-            <div className="img-holder">
-              <img src={`${props.user.carilla}`} alt="" />
+  return (
+    <NavLink to={`/songsDetails/`}>
+      <div className="card">
+        <div className="img-holder">
+          <img src={`${props.user.carilla}`} alt="" />
+        </div>
+        <div className="text">
+          <h2>{props.user.titulo} </h2>
+          <p>{props.user.nombre}</p>
+        </div>
+        <NavLink to={`/`}>
+          <div className="play-icon">
+            <div className="circle">
+              <div className="triangle"></div>
             </div>
-            <div className="text">
-              <h2>{props.user.titulo} </h2>
-              <p>{props.user.nombre}</p>
-            </div>
-            <NavLink to={`/`}>
-              <div className="play-icon">
-                <div className="circle">
-                  <div className="triangle"></div>
-                </div>
-              </div>
-              </NavLink>
           </div>
-          </NavLink>
-      );
-}
+        </NavLink>
+      </div>
+    </NavLink>
+  );
+};
 
-export default CancionUsuario
+export default CancionUsuario;
